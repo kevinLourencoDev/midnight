1cbe067d288b268a2848331bb21e5edf74364657
Kevin Lourenco <k.lourenco@criteo.com>
2020-02-25 00:41:49 +0100

    feat: new library architecture

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -4,10 +4,10 @@ import logo from './logo.svg';
 import './App.css';
 const App: React.FC = () => {
 	return (
-		<div className="App">
-			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
-				<Demo type="success">HELLO World</Demo>
+		<div className='App'>
+			<header className='App-header'>
+				<img src={logo} className='App-logo' alt='logo' />
+				<Demo type='success'>HELLO World</Demo>
 			</header>
 		</div>
 	);
